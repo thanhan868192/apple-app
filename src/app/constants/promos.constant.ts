49edaadee227b9cec7968e01aca71593179b78e1
anthanh <thanhan.lan@gmail.com>
2025-08-08 23:44:24 +0700

    test scroll submenu

---
+++ src/app/constants/promos.constant.ts
@@ -0,0 +1,75 @@
+export const PROMOS_DATA: any[] = [
+  {
+    bg: 'promo_ipadpro_avail__s271j89g8kii_large.jpg',
+    height: 560,
+    title: 'iPad Pro',
+    subtitle: 'Unbelievably thin. Incredibly powerful.',
+    buttons: [
+      { label: 'Learn more', type: 'primary' },
+      { label: 'Buy', type: 'outline' }
+    ],
+    note: 'Built for Apple Intelligence.',
+    layout: 'center-between',
+    textColor: 'white'
+  },
+  {
+    bg: 'promo_chatgpt__ezitunjy33u6_large.jpg',
+    height: 560,
+    title: 'Apple Intelligence',
+    subtitle: 'Point. Shoot. Cook. <br>With visual intelligence.<sup aria-label="Footnote 2">2</sup>',
+    buttons: [
+      { label: 'Watch the clip', type: 'primary' },
+      { label: 'Lean more', type: 'outline' }
+    ],
+    layout: 'center-end',
+    textColor: 'white'
+  },
+  {
+    bg: 'promo_airpods_pro_2_avail__vkitqi3okwie_large.jpg',
+    height: 560,
+    title: 'AirPods Pro 2',
+    subtitle: 'Now with a Hearing Aid feature.<sup aria-label="Footnote 3">3</sup>',
+    buttons: [
+      { label: 'Learn more', type: 'primary' },
+      { label: 'Buy', type: 'outline' }
+    ],
+    layout: 'center-end',
+    textColor: 'white'
+  },
+  {
+    bg: 'promo_apple_watch_series_10_avail_lte__c70y29goir42_large.jpg',
+    height: 560,
+    imgTitle: 'promo_logo_apple_watch_series_10__qk5vaa89vnm2_large.png',
+    subtitle: 'Thinstant classic.',
+    buttons: [
+      { label: 'Learn more', type: 'primary' },
+      { label: 'Buy', type: 'outline' }
+    ],
+    layout: 'center',
+    textColor: 'black'
+  },
+  {
+    bg: 'promo_iphone_tradein__bugw15ka691e_large.jpg',
+    height: 560,
+    imgTitle: 'promo_logo_iphone_tradein__7y3gtai5az66_large.png',
+    subtitle: 'Get $160-$600 in credit when you trade<br> in iPhone 12 or higher.<sup aria-label="Footnote 4">4</sup>',
+    buttons: [
+      { label: 'Learn more', type: 'primary' },
+      { label: 'Buy', type: 'outline' }
+    ],
+    layout: 'center',
+    textColor: 'black'
+  },
+  {
+    bg: 'promo_apple_card__5cm7draujpey_large.jpg',
+    height: 560,
+    imgTitle: 'logo__dcojfwkzna2q_large.png',
+    subtitle: 'Get up to 3% Daily Cash back<br> with every purchase.',
+    buttons: [
+      { label: 'Learn more', type: 'primary' },
+      { label: 'Buy', type: 'outline' }
+    ],
+    layout: 'center',
+    textColor: 'black'
+  }
+]
--- src/app/home/promos/promos.ts
+++ src/app/home/promos/promos.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PROMOS_DATA } from '../../constants/promos.constant';
 
 @Component({
   selector: 'app-promos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './promos.html',
   styleUrl: './promos.scss'
 })
 export class Promos {
-
+  promos = PROMOS_DATA;
 }
