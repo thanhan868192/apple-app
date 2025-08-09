51b03591e0a52bc25e9d0bbb1dc400b369836073
anthanh <thanhan.lan@gmail.com>
2025-08-09 16:50:39 +0700

    update UI

---
+++ src/app/constants/get-to-know.constant.ts
@@ -0,0 +1,50 @@
+export const GET_TO_KNOW_ITEMS: any[] = [
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'camera__exi2qfijti0y_medium_2x._01jpg.jpg',
+        light: false,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'apple_intelligence__gbh77cvflkia_medium_2x_02.jpg',
+        light: false,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'battery__2v7w6kmztvm2_medium_2x_03.jpg',
+        light: false,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'innovation__os9bmmo3mjee_medium_2x_04.jpg',
+        light: true,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'environment__e3v3gj88dl6q_medium_2x_05.jpg',
+        light: true,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'personalize__dwg8srggrbo2_medium_2x_06.jpg',
+        light: false,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'safety__bwp7rsowtjiu_medium_2x_07.jpg',
+        light: false,
+    },
+    {
+        title: 'AI-opening possibilities.',
+        subtitle: 'Apple Intelligence',
+        placeholder: 'privacy__ckc0wa30o55y_medium_2x_08.jpg',
+        light: false,
+    },
+];
--- src/app/home/get-to-know/get-to-know.ts
+++ src/app/home/get-to-know/get-to-know.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GET_TO_KNOW_IMPORTS } from '../../shared/get-to-know.import';
+import { GET_TO_KNOW_ITEMS } from '../../constants/get-to-know.constant';
 
 @Component({
   selector: 'app-get-to-know',
@@ -8,56 +9,7 @@ import { GET_TO_KNOW_IMPORTS } from '../../shared/get-to-know.import';
   styleUrl: './get-to-know.scss'
 })
 export class GetToKnow {
-  cards = [
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'camera__exi2qfijti0y_medium_2x._01jpg.jpg',
-      light: false,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'apple_intelligence__gbh77cvflkia_medium_2x_02.jpg',
-      light: false,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'battery__2v7w6kmztvm2_medium_2x_03.jpg',
-      light: false,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'innovation__os9bmmo3mjee_medium_2x_04.jpg',
-      light: true,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'environment__e3v3gj88dl6q_medium_2x_05.jpg',
-      light: true,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'personalize__dwg8srggrbo2_medium_2x_06.jpg',
-      light: false,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'safety__bwp7rsowtjiu_medium_2x_07.jpg',
-      light: false,
-    },
-    {
-      title: 'AI-opening possibilities.',
-      subtitle: 'Apple Intelligence',
-      placeholder: 'privacy__ckc0wa30o55y_medium_2x_08.jpg',
-      light: false,
-    },
-  ];
+  cards = GET_TO_KNOW_ITEMS;
 
   onFullLoaded(ev: Event) {
     const img = ev.target as HTMLImageElement;
